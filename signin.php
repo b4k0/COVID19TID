f38489cb61c2c11e48f899709e2d0a318a9cb2a9
Mpak1996 <giannismpak@outlook.com>
2022-02-04 20:36:29 +0200

    Update to signin and signup and boost conditionals for blank inputs and password

--- signin.php
+++ signin.php
@@ -1,4 +1,15 @@
-<?php
+<!-- HTML Code -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>COVID19 TID</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="container" id="container">
+	<?php
 
 $conn = new mysqli("localhost", "root", "", "covid19");
 
@@ -8,47 +19,104 @@ if($conn->connect_error){
 
 $username = $_POST["username"];
 $password = $_POST["password"];
-$number = preg_match('@[0-9]@', $password);
-$uppercase = preg_match('@[A-Z]@', $password);
-$lowercase = preg_match('@[a-z]@', $password);
-$specialChars = preg_match('@[^\w]@', $password);
 
-if(isset($username) && isset($password)){
-
-	if(empty($username)){
-		header("Location: index.php?blank=Username is required!");
-		exit();
-	}else if (empty($password)){
-		header("Location: index.php?blank=Password is required!");
-		exit();
-	}else{
-	header("Location: index.php");
-	exit();}
-}
 
 
 $sql = mysqli_query($conn, "SELECT * from users WHERE username = '$username' and password = '$password'");
 
 $row = mysqli_fetch_array($sql);
 
-if($row > 0){
-	?>
-	<script>
-		alert('Login successful');
-	</script>
-	
-	<?php
+if(isset($username) && isset($password)){
+
+	if(empty($username) && empty($password)){
+		header("Location: signin.php?error=Username and Password is required!");
+		exit();
+	}else if (empty($password)){
+		header("Location: signin.php?error=Password is required!");
+		exit();
+	}else if(empty($username)){
+		header("Location: signin.php?error=Username is required!");
+		exit();
+	}else{
+		if($row > 0){
+			if ($username=="admin" && $password=="admin"){
+				header("Location: adminDashboard.php");
+				exit();
+			}else{
+				header("Location: userDashboard.php");
+				exit();
+			}
+		}
+		else{
+			header("Location: signin.php?error=False Credentials!");
+			exit();
+		}
+	}
 }
-else{
-	header("Location: index.php?blank=False Credentials!");
-	exit();
-}
-
-
-
-
-
-
-
-
 ?>
+
+<div class="form-container sign-in-container">
+	<form action="signin.php" method="POST">
+		<h1>Sign In</h1>
+		<?php if (isset($_GET['error'])){ ?>
+			<p class="error"><?php echo $_GET['error']; ?></p>
+		<?php } ?>
+		<br>
+		<br>
+		<br>
+		<input type="text" name="username" placeholder="Username">
+		<input type="password" name="password" placeholder="Password">
+		<br>
+		<button>Sign In</button>
+	</form>
+</div>
+<div class="overlay-container">
+	<div class="overlay">
+		<div class="overlay-panel overlay-right">
+			<h1>COVID19 TID</h1>
+			<h1>Application</h1>
+			<br>
+			<h4>Web application for traffic inflation data for COVID-19 dispersion control.</h4>
+			<br>
+			<p>Do you want to visit any place with safety?</p>
+			<a href="signup.php">
+ 			 <button>Sign up</button>
+			</a>
+		</div>
+	</div>
+</div>
+</div>
+<p class="text_center">2022 All rights reserved, COVID19 TID Application <a href="signin.php"></a></p>
+ 
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- signup.php
+++ signup.php
@@ -1,4 +1,16 @@
-<?php  
+<!-- HTML Code -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>COVID19 TID</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="container" id="container">
+
+	<?php  
 
 $conn = new mysqli("localhost", "root", "", "covid19");
 
@@ -10,64 +22,106 @@ $email = $_POST["email"];
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-if(isset($username) && isset($password) && isset($email)){
 
-	if(empty($username)){
-		header("Location: index.php?error=Username is required!");
-		exit();
-	}else if (empty($password)){
-		header("Location: index.php?error=Password is required!");
-		exit();
-	}else if (empty($email)){
-		header("Location: index.php?error=Email is required!");
-		exit();
-	}else{
-		if(strlen($password) < 8 ) {
-			header("Location: index.php?error=Password must be at least 8 characters in length!");
-			exit();
-		} else if(!$number){
-			header("Location: index.php?error=Password must contain at least one number!");
-			exit();
-		}else if(!$uppercase){
-			header("Location: index.php?error=Password must contain at least one upper case letter!");
-			exit();
-		}else if(!$lowercase){
-			header("Location: index.php?error=Password must contain at least one lower case letter!");
-			exit();
-		}else if(!$specialChars){
-			header("Location: index.php?error=Password must contain at least one special character!");
-			exit();
-		}else {
-			$msg = "Your password is strong.";
-		}
-		
-	}
-}else{
-	header("Location: index.php");
-	exit();
-}
 
 
 $sql = "INSERT INTO users (email, username, password) 
 VALUES ('$email','$username', '$password')";
 
-if($conn->query($sql) === TRUE && !empty($username)  && !empty($password)){
-	?>
-	<script>
-		alert('Values have been inserted');
-	</script>
-	<?php
+if(isset($username) && isset($password) && isset($email)){
+	if(empty($username) && empty($password) && empty($email)){
+		header("Location: signup.php?error=Email,Username and Password is required!");
+		exit();
+	}else if(empty($username) && empty($password)){
+		header("Location: signup.php?error=Username and Password is required!");
+		exit();
+	}else if(empty($username) && empty($email)){
+		header("Location: signup.php?error=Username and Email is required!");
+		exit();
+	}else if(empty($email) && empty($password)){
+		header("Location: signup.php?error=Email and Password is required!");
+		exit();
+	}else if(empty($username)){
+		header("Location: signup.php?error=Username is required!");
+		exit();
+	}else if (empty($password)){
+		header("Location: signup.php?error=Password is required!");
+		exit();
+	}else if (empty($email)){
+		header("Location: signup.php?error=Email is required!");
+		exit();
+	}else{
+		if (isset($password)){
+		if(strlen($password) < 8 ) {
+			header("Location: signup.php?error=Password must be at least 8 characters in length!");
+			exit();
+		} else if(!$number){
+			header("Location: signup.php?error=Password must contain at least one number!");
+			exit();
+		}else if(!$uppercase){
+			header("Location: signup.php?error=Password must contain at least one upper case letter!");
+			exit();
+		}else if(!$lowercase){
+			header("Location: signup.php?error=Password must contain at least one lower case letter!");
+			exit();
+		}else if(!$specialChars){
+			header("Location: signup.php?error=Password must contain at least one special character!");
+			exit();
+		}else {
+			if($conn->query($sql) === TRUE){
+				alert('Values have been inserted');
+			}else{
+				alert('Values did not insert');}
+			
+			}
+		}
+		
+	}
 }
-else{
-	?>
-	<script>
-		alert('Values did not insert');
-	</script>
-	<?php
-}
+
+
 
 
 ?>
+<div class="form-container sign-in-container">
+<form action="signup.php" method="POST" >
+	<h1>Create Account</h1>
+	<?php if (isset($_GET['error'])){ ?>
+		<p class="error"><?php echo $_GET['error']; ?></p>
+	<?php } ?>
+	<br>
+	<br>
+	<br>
+	<input type="email" name="email" placeholder="Email">
+	<input type="text" name="username" placeholder="Username">
+	<input type="password" name="password" placeholder="Password"> 
+	<br>
+	<button>SignUp</button>
+</form>
+</div>
+<div class="overlay-container">
+	<div class="overlay">
+		<div class="overlay-panel overlay-right">
+			<h1>COVID19 TID</h1>
+			<h1>Application</h1>
+			<br>
+			<h4>Web application for traffic inflation data for COVID-19 dispersion control.</h4>
+			<br>
+			<p>To keep connected with us please login in our application!</p>
+			<a href="signin.php">
+ 			 <button>Sign in</button>
+			</a>
+		</div>
+	</div>
+</div>
+</div>
+
+
+<p class="text_center">2022 All rights reserved, COVID19 TID Application <a href="signup.php"></a></p>
+ 
+</body>
+</html>
+
 
 
 
